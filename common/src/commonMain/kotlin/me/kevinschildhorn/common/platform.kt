06f5ef136069d72e584ca55d3b01e5d58c594a14
Kevin Schildhorn <kevins@touchlab.co>
2022-09-06 11:32:30 -0400

    Adding initial Project

---
+++ common/src/commonMain/kotlin/me/kevinschildhorn/common/platform.kt
@@ -0,0 +1,3 @@
+package me.kevinschildhorn.common
+
+expect fun getPlatformName(): String
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+    
+}
+rootProject.name = "FotoPresenter"
+
+
+include(":android")
+include(":common")
+
