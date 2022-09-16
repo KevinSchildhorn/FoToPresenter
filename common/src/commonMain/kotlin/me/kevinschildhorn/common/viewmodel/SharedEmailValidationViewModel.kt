a327931cb59003a862c4a568f3a6375a28785022
Kevin Schildhorn <kevins@touchlab.co>
2022-09-16 09:59:35 -0400

    moving state to shared

--- common/src/commonMain/kotlin/me/kevinschildhorn/common/viewmodel/SharedEmailValidationViewModel.kt
+++ common/src/commonMain/kotlin/me/kevinschildhorn/common/viewmodel/SharedEmailValidationViewModel.kt
@@ -1,6 +1,10 @@
 package me.kevinschildhorn.android.viewModel
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.kevinschildhorn.common.ui.ColorOption
 import me.kevinschildhorn.common.uilogic.TextFieldState
 import me.kevinschildhorn.common.viewmodel.base.ViewModel
@@ -15,23 +19,25 @@ enum class EmailValidationState {
 open class SharedEmailValidationViewModel : ViewModel() {
     private val simpleEmailRegex = "^\\S+@\\S+\\.\\S+\$".toRegex()
 
-    var _validationState: MutableStateFlow<EmailValidationState> =
+    private var _validationState: MutableStateFlow<EmailValidationState> =
         MutableStateFlow(EmailValidationState.Empty)
     val validationState: StateFlow<EmailValidationState> = _validationState
+    
+    val emailTextFieldState: StateFlow<TextFieldState> = MutableStateFlow(TextFieldState(hint = "Email", defaultColor = ColorOption.NORMAL))
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = TextFieldState(hint = "Email", defaultColor = ColorOption.NORMAL)
+        )
 
-    val emailTextFieldState: StateFlow<TextFieldState> = MutableStateFlow(
-        TextFieldState(hint = "Email", defaultColor = ColorOption.NORMAL)
-    ).stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5_000),
-        initialValue = TextFieldState(hint = "Email", defaultColor = ColorOption.NORMAL)
-    )
 
     fun verifyEmail(email: String) {
-        this._validationState.value = when {
+        val nextState = when {
             email.isEmpty() -> EmailValidationState.Empty
             simpleEmailRegex.matches(email) -> EmailValidationState.Valid
             else -> EmailValidationState.Invalid
         }
+        _validationState.value = nextState
+        emailTextFieldState.value.updateWithState(nextState, email)
     }
 }
