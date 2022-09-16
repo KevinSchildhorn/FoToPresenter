ebe17951de8f5bcaf1c8cf7c1279a76a242383e0
Kevin Schildhorn <kevins@touchlab.co>
2022-09-16 13:04:17 -0400

    Adding iOS version

--- ios/FotoPresenter/ViewModel/EmailVerificationViewModel.swift
+++ ios/FotoPresenter/ViewModel/EmailVerificationViewModel.swift
@@ -19,13 +19,12 @@ class EmailVerificiationViewModel : ObservableObject {
             sharedViewModel.updateUsername(input: email)
         }
     }
-    //@Published private(set) var emailTextFieldState: TextFieldState = TextFieldState.companion.create(hint: "Email", defaultColor: ColorOption.normal)
+    @Published private(set) var emailTextFieldState: TextFieldState = TextFieldState.companion.create(hint: "Email", defaultColor: ColorOption.normal)
 
     init() {
         self.sharedViewModel = EmailValidationCallbackViewModel()
         observe(sharedViewModel.email.subscribe(onEach: { [weak self] in self?.email = $0 as String }))
-        //observe(sharedViewModel.emailTextFieldState.subscribe(onEach: { [weak self] in self?.emailTextFieldState = $0 }))
-
+        observe(sharedViewModel.emailTextFieldState.subscribe(onEach: { [weak self] in self?.emailTextFieldState = $0 }))
     }
 
     private func observe(_ cancellable: Canceller) {
