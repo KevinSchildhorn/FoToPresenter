18e29e648e3b4bbc4ba38608e4841a496b29f2df
Kevin Schildhorn <kevins@touchlab.co>
2022-09-19 12:40:49 -0400

    updating to use CombineAdapter

--- ios/FotoPresenter/ViewModel/EmailVerificationViewModel.swift
+++ ios/FotoPresenter/ViewModel/EmailVerificationViewModel.swift
@@ -8,10 +8,11 @@
 import Foundation
 import SharedFotoSDK
 import SwiftUI
+import Combine
 
 class EmailVerificiationViewModel : ObservableObject {
     var sharedViewModel: EmailValidationCallbackViewModel
-    private var cancellers: [Canceller] = []
+    private var cancellers: [Cancellable] = []
 
 
     @Published var email: String = "" {
@@ -23,12 +24,11 @@ class EmailVerificiationViewModel : ObservableObject {
 
     init() {
         self.sharedViewModel = EmailValidationCallbackViewModel()
-        observe(sharedViewModel.email.subscribe(onEach: { [weak self] in self?.email = $0 as String }))
-        observe(sharedViewModel.emailTextFieldState.subscribe(onEach: { [weak self] in self?.emailTextFieldState = $0 }))
-    }
 
-    private func observe(_ cancellable: Canceller) {
-        cancellers.append(cancellable)
+        cancellers += [
+            doPublish(sharedViewModel.email, onEach: { [weak self] in self?.email = $0 as String }),
+            doPublish(sharedViewModel.emailTextFieldState, onEach: { [weak self] in self?.emailTextFieldState = $0 })
+        ]
     }
 
     deinit {
