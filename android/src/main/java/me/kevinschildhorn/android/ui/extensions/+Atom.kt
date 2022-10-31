d3e2b47ff7ce2dc20a0cab26b7d7df703f21709d
Kevin Schildhorn <kevins@touchlab.co>
2022-10-31 10:20:41 -0400

    updating

--- android/src/main/java/me/kevinschildhorn/android/ui/extensions/+Atom.kt
+++ android/src/main/java/me/kevinschildhorn/android/ui/extensions/+Atom.kt
@@ -3,17 +3,17 @@ package me.kevinschildhorn.android.util
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import me.kevinschildhorn.android.navigationbutton.Button
 import me.kevinschildhorn.android.ui.extensions.asModifier
 import me.kevinschildhorn.common.ui.atomic.atoms.Atom
 import me.kevinschildhorn.common.ui.atomic.atoms.AtomType
 import me.kevinschildhorn.common.ui.atomic.designproperty.*
-
+/*
 @Composable
 fun Atom.AsComposable(text: String) {
     when (type) {
@@ -21,7 +21,7 @@ fun Atom.AsComposable(text: String) {
         AtomType.TEXT -> Text(text = text, modifier = design.modifiers)
         AtomType.GROUP -> Box(modifier = design.modifiers.fillMaxSize(1.00f)) {}
     }
-}
+}*/
 
 val List<DesignProperty>.modifiers: Modifier
     get() {
