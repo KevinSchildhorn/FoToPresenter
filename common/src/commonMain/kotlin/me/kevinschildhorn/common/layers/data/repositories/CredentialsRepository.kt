77f5c66adcd91a2f6068d45ab6f54dbe6053d339
Kevin Schildhorn <kevins@touchlab.co>
2022-11-09 13:57:42 -0500

    Adding koin

--- common/src/commonMain/kotlin/me/kevinschildhorn/common/layers/data/repositories/CredentialsRepository.kt
+++ common/src/commonMain/kotlin/me/kevinschildhorn/common/layers/data/repositories/CredentialsRepository.kt
@@ -1,4 +1,11 @@
 package me.kevinschildhorn.common.layers.data.repositories
 
-class CredentialsRepository {
+import me.kevinschildhorn.common.layers.data.datasources.CredentialsDataSource
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class CredentialsRepository : KoinComponent {
+    val dataSource: CredentialsDataSource by inject()
+
+
 }
