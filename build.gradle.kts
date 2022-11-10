33e1d93171294226c0e2478b137a328a7b9ebea6
Kevin Schildhorn <kevins@touchlab.co>
2022-11-10 17:58:42 -0500

    Adding viewmodel to swift

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
-        classpath(kotlin("gradle-plugin", "1.7.10"))
+        classpath(kotlin("gradle-plugin", "1.7.20"))
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.3.0-alpha01-dev849")
     }
 }
