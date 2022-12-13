913062e4fa711d329e6aaa2e4b66e5178adb5471
Kevin Schildhorn <kevins@touchlab.co>
2022-12-13 13:52:39 -0500

    Adding Kermit to project

--- common/src/commonMain/kotlin/me/kevinschildhorn/common/koin.kt
+++ common/src/commonMain/kotlin/me/kevinschildhorn/common/koin.kt
@@ -1,5 +1,7 @@
 package me.kevinschildhorn.common
 
+import co.touchlab.kermit.Logger
+import co.touchlab.kermit.LoggerConfig
 import me.kevinschildhorn.common.architecture.data.datasources.CredentialsDataSource
 import me.kevinschildhorn.common.architecture.data.repositories.CredentialsRepository
 import me.kevinschildhorn.common.architecture.domain.SaveCredentialsUseCase
@@ -7,10 +9,20 @@ import me.kevinschildhorn.common.architecture.ui.viewmodel.LoginViewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
+internal fun kermitModule(
+    baseLogger: Logger,
+) = module {
+    factory { (tag: String?) ->
+        if (tag != null) baseLogger.withTag(tag) else baseLogger
+    }
+}
+
 val commonModule = module {
+    val baseLogger = Logger(LoggerConfig.default)
+
     single { CredentialsDataSource(get()) }
     single { CredentialsRepository(get()) }
-    factory { SaveCredentialsUseCase(get()) }
+    factory { SaveCredentialsUseCase(get(), baseLogger.withTag("SaveCredentialsUseCase")) }
     single { LoginViewModel(get()) }
 }
 
