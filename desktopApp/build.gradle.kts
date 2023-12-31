5015624aed8fde8bde163f6396db544ac10eccdd
Kevin Schildhorn <kevins@touchlab.co>
2023-12-31 15:21:29 -0500

    Updating settings

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -12,6 +12,8 @@ kotlin {
             dependencies {
                 implementation(compose.desktop.currentOs)
                 implementation("io.insert-koin:koin-core:3.4.0")
+                implementation("co.touchlab:kermit:1.2.2")
+                implementation("com.russhwolf:multiplatform-settings:1.0.0")
 
                 implementation(project(":shared"))
             }
