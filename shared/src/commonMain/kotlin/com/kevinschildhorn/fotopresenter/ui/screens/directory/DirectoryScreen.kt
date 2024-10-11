93ee04cda874573e7cb21ea4b5f49f9066b4ad0a
Kevin Schildhorn <kevin.schildhorn@gmail.com>
2024-10-11 13:30:48 -0400

    removing loggedIn

--- shared/src/commonMain/kotlin/com/kevinschildhorn/fotopresenter/ui/screens/directory/DirectoryScreen.kt
+++ shared/src/commonMain/kotlin/com/kevinschildhorn/fotopresenter/ui/screens/directory/DirectoryScreen.kt
@@ -69,7 +69,6 @@ fun DirectoryScreen(
     var overlayVisible by remember { mutableStateOf(DirectoryOverlay.NONE) }
 
     // Navigation
-    if (!uiState.loggedIn) onLogout()
     uiState.slideshowDetails?.let {
         onStartSlideshow(it)
     }
