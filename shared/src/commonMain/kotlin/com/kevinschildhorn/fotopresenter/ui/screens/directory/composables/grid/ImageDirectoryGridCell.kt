9ad20866cb55b2eaadc5122ddb34f635a28e0836
Kevin Schildhorn <kevin.schildhorn@gmail.com>
2024-12-11 16:19:40 -0500

    Adding placeholder images

--- shared/src/commonMain/kotlin/com/kevinschildhorn/fotopresenter/ui/screens/directory/composables/grid/ImageDirectoryGridCell.kt
+++ shared/src/commonMain/kotlin/com/kevinschildhorn/fotopresenter/ui/screens/directory/composables/grid/ImageDirectoryGridCell.kt
@@ -1,13 +1,18 @@
 package com.kevinschildhorn.fotopresenter.ui.screens.directory.composables.grid
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import coil3.compose.AsyncImage
+import com.kevinschildhorn.fotopresenter.Res
+import com.kevinschildhorn.fotopresenter.error
+import com.kevinschildhorn.fotopresenter.photo_camera
 import com.kevinschildhorn.fotopresenter.ui.atoms.fotoColors
 import com.kevinschildhorn.fotopresenter.ui.screens.directory.DirectoryGridCellState
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun ImageDirectoryGridCell(
@@ -20,6 +25,8 @@ fun ImageDirectoryGridCell(
             contentDescription = imageContent.name,
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize().background(fotoColors.surface),
+            error = painterResource(Res.drawable.error),
+            placeholder = painterResource(Res.drawable.photo_camera),
         )
     }
 }
